5c7843ababbe0e5bc132317b8d4a03abeab3c937
tanviAlam <tanvir9832@gmail.com>
2024-12-10 15:06:15 +0600

    added

--- src/db.js
+++ src/db.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 exports.databaseConnection = async () => {
   try {
-    console.log(process.env.DATABASE_URL)
-    await mongoose.connect(process.env.DATABASE_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      process.env.DATABASE_URL ||
+        "mongodb+srv://dadubari2023:12345678abcDEF@atlascluster.bkiupxh.mongodb.net/productCrud",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("database connected!");
   } catch (error) {
     console.log(error.message || "database connection failed");
