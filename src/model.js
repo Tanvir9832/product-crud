b0b376650500bf57f38207681518f6e2ecff5691
tanviAlam <tanvir9832@gmail.com>
2024-12-10 13:29:51 +0600

    feat: product model added

--- src/model.js
+++ src/model.js
@@ -1,6 +1,17 @@
-const mongose =  require("mongoose");
+const mongoose = require("mongoose");
 
+const product = new mongoose.Schema(
+  {
+    productName: String,
+    productDetails: String,
+    price: Number,
+    quantity: Number,
+    totalPrice: Number,
+    productImage: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const product = new mongoose.Schema({
-
-})
+module.exports = new mongoose.model("Product", product);
