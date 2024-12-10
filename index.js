c3fe86d97661d6f2abe92ec931891ec7b4bb9788
tanviAlam <tanvir9832@gmail.com>
2024-12-10 14:32:05 +0600

    feat: deploy

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/", productRouter);
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html")),
+  res.sendFile(path.join(__dirname, "/frontend/dist/index.html")),
     function (err) {
       res.status(500).send(err);
     };
