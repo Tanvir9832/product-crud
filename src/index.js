083961bffe01092fded222acf78da08a7f149c09
tanviAlam <tanvir9832@gmail.com>
2024-12-10 14:20:31 +0600

    feat: deployment

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ const app = express();
 require("dotenv").config("../env");
 const cors = require("cors");
 const productRouter = require("./router");
+const path = require("path");
 
 app.use(express.json());
 app.use(cors());
 app.use("/api/", productRouter);
-app.use("*", (req, res) => {
-  res.status(404).json({
-    issue: "wrong url",
-  });
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html")),
+    function (err) {
+      res.status(500).send(err);
+    };
 });
 
 databaseConnection();
