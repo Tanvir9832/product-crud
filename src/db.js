e47d9d0671ebd6686dbd16fefb3cc8bb0eee7e92
tanviAlam <tanvir9832@gmail.com>
2024-12-10 15:01:26 +0600

    fix : check

--- src/db.js
+++ src/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 exports.databaseConnection = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
+    console.log(process.env.DATABASE_URL)
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("database connected!");
   } catch (error) {
     console.log(error.message || "database connection failed");
